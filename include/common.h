4f0810fdcb25cd06dc2769add74137ea9a56a987
freg <549642238@qq.com>
2020-09-21 22:44:12 +0800

    Add ASSERT

--- include/common.h
+++ include/common.h
@@ -5,6 +5,15 @@ extern int control_msg_level;
 
 #define VERSION "1.0"
 
+#define ASSERT(exp)							\
+	do {								\
+		if (!(exp)) {						\
+			printf("ASSERT [%s:%4d] (%s) failed\n",		\
+				__FILE__, __LINE__, #exp);		\
+			exit(-1);					\
+		}							\
+	} while(0)
+
 #define MSG(level, format, ...)						\
 	do {								\
 		if (level >= control_msg_level) {			\
